bee3d4c3b18e1005730a07b476cef401562afe70
Mehmood <Mehmood3980350@gmail.com>
2025-05-31 10:50:51 +0500

    Add transaction record function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import os
+from datetime import datetime
 
 DATA_FILE = "transactions.csv"
 
@@ -9,6 +10,13 @@ def initialize_csv():
             writer = csv.writer(file)
             writer.writerow(["date", "type", "category", "amount", "description"])
 
+def add_transaction(t_type, category, amount, description):
+	date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	with open(DATA_FILE, 'a', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow([date, t_type, category, amount, description])
+
 if __name__ == "__main__":
     initialize_csv()
+    add_transaction("income", "salary", 1000, "Monthly salary")
     print("Personal Finance Tracker initialized.")
